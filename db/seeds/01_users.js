f3f084afa0f015ade31c0f2acb775c0d63400b92
Rawle Sekhon <amandeepsekhon404@gmail.com>
2018-11-14 21:35:42 +0000

    changed the order of seeds

--- db/seeds/01_users.js
+++ db/seeds/01_users.js
@@ -4,18 +4,21 @@ exports.seed = function(knex, Promise) {
     .then(function() {
       return Promise.all([
         knex("users").insert({
+          id: 1,
           first_name: "Alice",
           last_name: "wonderland",
           phone: "5555555555",
           email: "me@me.com"
         }),
         knex("users").insert({
+          id: 2,
           first_name: "Bob",
           last_name: "bill",
           phone: "2222222222",
           email: "me@me.com"
         }),
         knex("users").insert({
+          id: 3,
           first_name: "Charlie",
           last_name: "chocolate-factory",
           phone: "888888888",
