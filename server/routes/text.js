88d3d8035ec8976057e02034cffe5423b4e7c8ad
Anton Mihail Lachmaniucu <antonmihail@gmail.com>
2018-11-15 15:22:07 -0500

    Added texting notification

--- server/routes/text.js
+++ server/routes/text.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const router = express.Router();
+// Your Account Sid and Auth Token from twilio.com/console
+const accountSid = process.env.ACCOUNT_SID;
+const authToken = process.env.AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
 
 function handleInboundSms(request, response) {
   const params = Object.assign(request.query, request.body)
@@ -12,6 +16,14 @@ module.exports = (() => {
     req.on('data', (e) => {
       e = JSON.parse(e);
       console.log(e.number, e.message);
+      client.messages
+        .create({
+          body: e.message,
+          from: '+16474934161',
+          to: '+1' + e.number,
+        })
+        .then(message => console.log(message.sid))
+        .done();
     })
   });
 
@@ -22,4 +34,4 @@ module.exports = (() => {
 
 
   return router;
-})()
+})();
