e9c2d53846129598a632cf2eace981d29d4707b3
Joseph Pham <j.pham017@gmail.com>
2018-11-15 16:48:22 -0500

    added cart functionality

--- frontend/public/scripts/cart.js
+++ frontend/public/scripts/cart.js
@@ -1,12 +1,18 @@
-const items = [];
+cartItems = ( typeof cartItems != 'undefined' && cartItems instanceof Array ) ? cartItems : []
 
-$(".add-cart").on( "click", function() {
+$('.add-cart').on("click", function() {
     let div = $(this).closest("div")
     let addedItem = {
         name : div.find(".product").text(),
         description : div.find(".description").text(),
         price : div.find(".price").text()
     }
-    items.push(addedItem);
-    console.log(items);
+    cartItems.push(addedItem);
+    console.log(cartItems);
+    updateCart(cartItems);
 });
+
+function updateCart(items){
+    let $count = $('.counter');
+    $count.text(items.length)
+}
