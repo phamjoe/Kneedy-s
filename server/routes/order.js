03c4589f4ef16ade8b209c952a2d1a11edc6383c
Anton Mihail Lachmaniucu <antonmihail@gmail.com>
2018-11-14 19:22:58 -0500

    Created order routes

--- server/routes/order.js
+++ server/routes/order.js
@@ -1,1 +1,17 @@
 "use strict";
+
+const express = require('express');
+const router = express.Router();
+
+
+module.exports = (() => {
+  router.get('/', (req, res) => {
+
+  });
+  router.post('/', (req, res) => {
+    const user_id = req.data.user_id;
+    const options = JSON.parse(req.data.options);
+  });
+
+  return router;
+})()
