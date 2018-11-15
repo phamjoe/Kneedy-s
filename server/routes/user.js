98908e778a7813efc876f48faba554ee41ad1faa
Rawle Sekhon <amandeepsekhon404@gmail.com>
2018-11-15 00:16:51 +0000

    added the user routes

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,13 +1,23 @@
 "use strict";
-
-const express = require('express');
+const {
+  add,
+  getPast,
+  getInfo,
+  verify
+} = require("./db/methods/user_interface.js");
+const express = require("express");
 const router = express.Router();
 
 module.exports = (() => {
-
-  router.get("/", (req, res) => {
-
+  router.get("/:id", (req, res) => {
+    res.json(JSON.stringify(getInfo(req.params.id)));
+  });
+  router.put("/:id", (req, res) => {
+    res.status(200);
   });
 
+  router.get("/:id/orders", (req, res) => {
+    res.json(JSON.stringify(getPast(req.params.id)));
+  });
   return router;
 })();
