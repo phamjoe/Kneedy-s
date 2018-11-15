f213f754bca5cda984b27b2d4844a2edb1d23041
Anton Mihail Lachmaniucu <antonmihail@gmail.com>
2018-11-14 19:01:40 -0500

    Created restful products endpoint

--- server/routes/products.js
+++ server/routes/products.js
@@ -2,13 +2,20 @@
 
 const express = require('express');
 const router = express.Router();
+const {
+  getIndividual,
+  getAll
+} = require('../db/methods/product_interface');
 const restaurantNumber = '14165222220';
 
 module.exports = (() => {
 
   router.get("/", (req, res) => {
-
+    res.json(JSON.stringify(getAll()));
   });
-
+  router.get('/:product_id', (req, res) => {
+    const productInfo = getIndividual(req.params.product_id);
+    res.json(JSON.stringify(productInfo));
+  });
   return router;
 })();
