27268d63285ba025c86ef4d51fda6ab33550c8a7
SylvainJunca <sylvain.junca@gmail.com>
2018-11-15 19:06:37 -0500

    the flag to check if an item is already in the cart works. Yay!

--- frontend/public/scripts/cart.js
+++ frontend/public/scripts/cart.js
@@ -5,8 +5,11 @@ $('.add-cart').on("click", function() {
     let addedItem = {
         name : div.find(".product").text(),
         description : div.find(".description").text(),
-        price : div.find(".price").text()
+        price : div.find(".price").text(),
+        id : div.find(".productID").text(),
+        quantity : 1
     }
+    checkCart(cartItems, addedItem);
     cartItems.push(addedItem);
     console.log(cartItems);
     updateCart(cartItems);
@@ -15,4 +18,26 @@ $('.add-cart').on("click", function() {
 function updateCart(items){
     let $count = $('.counter');
     $count.text(items.length)
+}
+
+
+
+// id {
+//     name : div.find(".product").text(),
+//     description : div.find(".description").text(),
+//     price : div.find(".price").text()
+// }
+
+function checkCart(cartItems, addedItem) {
+    let flag = 0
+    checkItem = addedItem.id
+    if (cartItems) {
+        for( let i = 0; i < cartItems.length; i++) {
+            if (cartItems[i]['id'] == checkItem) {
+                flag = 1
+            }
+        }
+    }
+    console.log(flag);
+    return(flag);
 }
