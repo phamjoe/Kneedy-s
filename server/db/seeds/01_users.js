29ef02bb4c3e9a5f5e268fac3667d26c403adc01
Rawle Sekhon <amandeepsekhon404@gmail.com>
2018-11-16 00:55:39 +0000

    updated seeds

--- server/db/seeds/01_users.js
+++ server/db/seeds/01_users.js
@@ -1,25 +1,49 @@
-exports.seed = function (knex, Promise) {
+exports.seed = function(knex, Promise) {
   return knex("users")
     .del()
-    .then(function () {
+    .then(function() {
       return Promise.all([
         knex("users").insert({
           first_name: "Alice",
           last_name: "wonderland",
-          phone: "5555555555",
-          email: "me@me.com"
+          phone: "4165222220",
+          email: "tinman@witch.com"
         }),
         knex("users").insert({
           first_name: "Bob",
           last_name: "bill",
-          phone: "2222222222",
-          email: "me@me.com"
+          phone: "2267007741",
+          email: "Bob@bill.com"
         }),
         knex("users").insert({
           first_name: "Charlie",
           last_name: "chocolate-factory",
-          phone: "888888888",
-          email: "me@me.com"
+          phone: "2267007741",
+          email: "Charlie@chocolate.com"
+        }),
+        knex("users").insert({
+          first_name: "Jayvon",
+          last_name: "Whitaker",
+          phone: "4165222220",
+          email: "Jayvon@Whitaker.com"
+        }),
+        knex("users").insert({
+          first_name: "Maribel",
+          last_name: "Ware",
+          phone: "2267007741",
+          email: "Maribel@Ware.com"
+        }),
+        knex("users").insert({
+          first_name: "Abraham",
+          last_name: "Merritt",
+          phone: "4165222220",
+          email: "Abraham@Merritt.com"
+        }),
+        knex("users").insert({
+          first_name: "Susan",
+          last_name: "Hines",
+          phone: "2267007741",
+          email: "Susan@Hines.com"
         })
       ]);
     });
