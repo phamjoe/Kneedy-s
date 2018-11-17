953923403a976df0fb9e618b510820c6de17399b
Rawle Sekhon <amandeepsekhon404@gmail.com>
2018-11-17 22:14:19 +0000

    pls

--- server/routes/order.js
+++ server/routes/order.js
@@ -1,23 +1,23 @@
 "use strict";
 
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const {
   addNewOrder: add,
   getOrderDetails: getDetails,
   updateOrder: update
-} = require('../db/methods/orders_interface');
+} = require("../db/methods/orders_interface");
 
-
+// routes for orders
 module.exports = (() => {
-  router.get('/:id', (req, res) => {
+  router.get("/:id", (req, res) => {
     getDetails(req.params.id).then((resolve, reject) => {
       if (reject) throw reject;
       res.json(resolve);
     });
   });
-  router.post('/', (req, res) => {
-    req.on('data', (e) => {
+  router.post("/", (req, res) => {
+    req.on("data", e => {
       e = JSON.parse(e);
       const user_id = e.user_id;
       const options = e.options;
--- server/routes/text.js
+++ server/routes/text.js
@@ -1,49 +1,48 @@
-require('dotenv').config();
+//Express
+require("dotenv").config();
 const express = require("express");
 const router = express.Router();
 // Your Account Sid and Auth Token from twilio.com/console
 const accountSid = process.env.ACCOUNT_SID;
 const authToken = process.env.AUTH_TOKEN;
-const client = require('twilio')(accountSid, authToken);
+const client = require("twilio")(accountSid, authToken);
 
+// Routes for handling the text messages.
 function handleInboundSms(request, response) {
-  const params = Object.assign(request.query, request.body)
+  const params = Object.assign(request.query, request.body);
   console.log(params.Body);
   client.messages
     .create({
       body: "It's gonna take " + params.Body + " minutes.",
-      from: '+16474934161',
-      to: '+12267007741',
+      from: "+16474934161",
+      to: "+12267007741"
     })
     .then(message => console.log(message.sid))
     .done(() => {
       response.status(204).send();
     });
-
 }
 
 module.exports = (() => {
-  router.post('/', (req, res) => {
-    req.on('data', (e) => {
+  router.post("/", (req, res) => {
+    req.on("data", e => {
       console.log(e);
       e = JSON.parse(e);
-      e.forEach((el) => {
+      e.forEach(el => {
         console.log(el.number, el.message);
         client.messages
           .create({
             body: el.message,
-            from: '+16474934161',
-            to: '+1' + el.number,
+            from: "+16474934161",
+            to: "+1" + el.number
           })
           .then(message => console.log(message.sid))
           .done(() => {});
       });
-    })
+    });
     res.end();
   });
-  router.post('/inbound-sms', handleInboundSms);
-
-
+  router.post("/inbound-sms", handleInboundSms);
 
   return router;
 })();
