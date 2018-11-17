f7624aa6c780fcbd4146500c7bf90de4568e77b0
Joseph Pham <j.pham017@gmail.com>
2018-11-17 13:05:01 -0500

    made changes to cart total

--- frontend/public/scripts/cart-update.js
+++ frontend/public/scripts/cart-update.js
@@ -13,14 +13,24 @@ const cart = (data) => {
       $(".product-remove").on("click", function () {
         let tr = $(this).closest("tr");
         let c = tr.children(".product-name");
+        let p = tr.children(".price");
+        let $tax = $(".tax");
+        let $total = $(".total");
         let val = c
           .children(".name")
           .text()
           .trim();
+        let price = p
+        .text()
+        .trim();
+        let subtotal = (($(".subtotal").text().substr(1)) - (price.substr(1))).toFixed(2);
+
         let count = $(".counter").text();
         count = parseInt(count) - 1;
         $(".counter").text(count);
-        console.log(val);
+        $(".subtotal").text(`$${subtotal}`);
+         $tax.text('$'+(subtotal * 0.13).toFixed(2));
+         $total.text('$'+(subtotal * 1.13).toFixed(2));
         $(tr).remove();
         $.ajax({
           method: "POST",
@@ -74,30 +84,27 @@ const buildProduct = (info) => {
     let priceTrim = element.quantity;
     let el = $(`
         <tr class="text-center">
-		<td class="product-remove"><button class="btn btn-outline-black"><span class="icon-close"></span></button></td>
-		<td class="image-prod">
-		<div class="img" style='background-image:url("${element.imgUrl}");'></div>
-        </td>
-    	<td class="product-name">
-		<h3 class="name">
-		${element.name}</h3>
-		<p class="description">${element.description}</p>
-	    </td>
-		<td class="price">${element.price}</td>
-		<td class="quantity">
-		<div class="input-group mb-3">
-		<input type="text" name="quantity" class="quantity form-control input-number" value="${
-      element.quantity
-    }" min="1" max="100">
-		</div>
-		</td>
-		<td class="item-total">$${element.price * priceTrim}</td>
+		    <td class="product-remove"><button class="btn btn-outline-black"><span class="icon-close"></span></button></td>
+		    <td class="image-prod">
+		        <div class="img" style='background-image:url("${element.imgUrl}");'></div>
+            </td>
+    	    <td class="product-name">
+		        <h3 class="name">${element.name}</h3>
+		        <p class="description">${element.description}</p>
+	        </td>
+		    <td class="price">$${element.price}</td>
+		    <td class="quantity">
+		        <div class="input-group mb-3">
+		            <input type="text" name="quantity" class="quantity form-control input-number" value="${element.quantity}" min="1" max="100">
+		        </div>
+		    </td>
+		    <td class="item-total">$${(element.price * priceTrim).toFixed(2)}</td>
 		</tr>`);
     objs.push(el);
     subtotal += element.price * priceTrim;
-    $subtotal.text(subtotal.toFixed(2));
-    $tax.text((subtotal * 0.13).toFixed(2));
-    $total.text((subtotal * 1.13).toFixed(2));
+    $subtotal.text('$' + subtotal.toFixed(2));
+    $tax.text('$'+(subtotal * 0.13).toFixed(2));
+    $total.text('$'+(subtotal * 1.13).toFixed(2));
   });
 
   return Promise.resolve(objs);
@@ -111,7 +118,6 @@ const renderEls = (els) => {
 
 $(".clear-cart").on("click", function () {
   $(".root").empty();
-  window.localStorage.clear();
 });
 
 if (window.cart) {
